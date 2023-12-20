8c366b2d78a5362944a46014a36e99c9412e68b3
hf <1264048312@qq.com>
2023-12-20 11:16:36 +0800

    修改 不支持 jsx

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,14 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+// import vueJsx from '@vitejs/plugin-vue-jsx'
 import dts from "vite-plugin-dts"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueJsx(),
+    // vueJsx(),
     dts()
   ],
   resolve: {
