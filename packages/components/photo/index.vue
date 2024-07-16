d801c82f3e2e37bddc16ea51001d5a210fe42153
hf <1264048312@qq.com>
2024-07-16 10:17:32 +0800

    不是文件服务器，文件大小限制10M

--- packages/components/photo/index.vue
+++ packages/components/photo/index.vue
@@ -87,13 +87,13 @@
     })
     return false
   }
-  const uploadExport = async (file: any) => { // 文件不是上传到文件服务时
-      // let maxSize = await config.getValue('MAX_FILE_SIZE')
-      // let maxSizes = maxSize * 1024 * 1024
-      // if (file.size && file.size > maxSizes) {
-      //   message.error(`上传文件不能大于${maxSize}M`, 3)
-      //   return
-      // }
+  const uploadExport = async (file: any) => { // 文件不是上传到文件服务时， 大小限制10M
+      let maxSize = 10
+      let maxSizes = maxSize * 1024 * 1024
+      if (file.size && file.size > maxSizes) {
+        message.error(`上传文件不能大于${maxSize}M`, 3)
+        return
+      }
       let form = new FormData()
       form.append('file', file)
       if (props.otherParam && Object.keys(props.otherParam).length > 0) {
